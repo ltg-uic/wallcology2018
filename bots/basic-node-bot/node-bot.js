dfe6ac59738b6997fd2bc70da27ebb9ae19c465d
tmoher <tmoher@users.noreply.github.com>
2018-09-16 13:29:57 -0500

    keeping clients alive

--- bots/basic-node-bot/node-bot.js
+++ bots/basic-node-bot/node-bot.js
@@ -1,21 +1,21 @@
-var NUTELLA = require('nutella_lib');
+// var NUTELLA = require('nutella_lib');
 
-// Get configuration parameters and init nutella
-var cliArgs = NUTELLA.parseArgs();
-var componentId = NUTELLA.parseComponentId();
-var nutella = NUTELLA.init(cliArgs.broker, cliArgs.app_id, cliArgs.run_id, componentId);
+// // Get configuration parameters and init nutella
+// var cliArgs = NUTELLA.parseArgs();
+// var componentId = NUTELLA.parseComponentId();
+// var nutella = NUTELLA.init(cliArgs.broker, cliArgs.app_id, cliArgs.run_id, componentId);
 // Optionally you can set the resource Id
-nutella.setResourceId('my_resource_id');
+// nutella.setResourceId('my_resource_id');
 
 
-console.log("Keep alive bot. Pings every minute. Interfaces must subscribe.");
+console.log("Does nothing. Should be deleted.");
 
-var pingcount = 0;
-// 2. Publish a message to a channel
-setInterval(ping, 60*1000); //ping every minute
+// var pingcount = 0;
+// // 2. Publish a message to a channel
+// setInterval(ping, 60*1000); //ping every minute
 
-function ping () {
- nutella.net.publish('ping',pingcount++);
-}
+// function ping () {
+//  nutella.net.publish('ping',pingcount++);
+// }
 
 
